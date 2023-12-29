9acb0a67277be3a4ffbeef4abf183d176460034d
Lucas Grunenberg <lucas.grunenberg@gmail.com>
2023-12-29 17:37:56 +0100

    Add PID settings and lock state monitoring

--- klippy/extras/servo42c.py
+++ klippy/extras/servo42c.py
@@ -13,9 +13,9 @@ ReadRegisters = {
     "ENC_VALUE": {"addr": 0x30, "len": 8},
     "PULSE_VALUE": {"addr": 0x33, "len": 6},
     "ERROR_VALUE": {"addr": 0x39, "len": 4},
+    "POS_VALUE": {"addr": 0x36, "len": 6},
     "EN_STATUS": {"addr": 0x3A, "len": 3},
-    "LOCK_STATE": {"addr": 0x3D, "len": 3},
-    "PROTECT_STATE": {"addr": 0x3E, "len": 3},
+    "LOCK_STATE": {"addr": 0x3E, "len": 3},
 }
 
 WriteRegisters = {
@@ -26,7 +26,8 @@ WriteRegisters = {
     "SET_EN": {"addr": 0x85, "len": 4},
     "SET_DIR": {"addr": 0x86, "len": 4},
     "SET_SDD": {"addr": 0x87, "len": 4},
-    "SET_LOCK": {"addr": 0x88, "len": 4},
+    "SET_PROTECT": {"addr": 0x88, "len": 4},
+    "SET_UNLOCK": {"addr": 0x3D, "len": 3},
     "SET_MPLYER": {"addr": 0x89, "len": 4},
     "RESTORE": {"addr": 0x3F, "len": 3},
     "SET_ADDR": {"addr": 0x8B, "len": 4},
@@ -67,7 +68,13 @@ class SERVO42C:
         self.stepper_name = " ".join(config.get_name().split()[1:])
         self.stepper_enable = self.printer.load_object(config, "stepper_enable")
         self.current = config.getfloat("current", above=0.0, maxval=MAX_CURRENT)
-        self.torque = config.getfloat("torque", above=0.0, maxval=MAX_TORQUE)
+        self.torque = config.getint(
+            "torque", default=450, minval=0, maxval=MAX_TORQUE
+        )
+
+        self.kp = config.getint("kp", default=None)
+        self.ki = config.getint("ki", default=None)
+        self.kd = config.getint("kd", default=None)
 
         gcode = self.printer.lookup_object("gcode")
         self.name = config.get_name().split()[-1]
@@ -102,6 +109,14 @@ class SERVO42C:
         try:
             self.set_current(self.current, None)
             self.set_torque(self.torque, None)
+
+            if self.kp is not None:
+                self.set_kp(self.kp, None)
+            if self.ki is not None:
+                self.set_ki(self.ki, None)
+            if self.kd is not None:
+                self.set_kd(self.kd, None)
+
         except self.printer.command_error as e:
             logging.info("Servo42C %s failed to init: %s", self.name, str(e))
 
@@ -114,21 +129,22 @@ class SERVO42C:
         self.current = val * 0.2
         # Register commands
 
-    def get_motor_protect(self):
-        return self.mcu_s42c.get_register("PROTECT_STATE")
-
     def get_motor_lock(self):
-        return self.mcu_s42c.get_register("LOCK_STATE")
+        logging.info("getting lock state")
+        lock_state = self.mcu_s42c.get_register("LOCK_STATE")
+        logging.info("Lock state is %s", str(lock_state))
+        if lock_state == bytearray(b"\x01"):
+            return True
+        return False
 
-    def set_motor_lock(self, state, print_time):
-        val = state
-        self.mcu_s42c.set_register("SET_LOCK", val, print_time)
+    def set_motor_unlock(self, print_time):
+        self.mcu_s42c.set_register("SET_UNLOCK", 0, print_time)
 
     def set_enable(self, state, print_time):
         val = 0
         if state:
             if self.get_motor_lock():
-                self.set_motor_lock(False, print_time)
+                self.set_motor_unlock(print_time)
             val = 1
         try:
             self.mcu_s42c.set_register("SET_EN", val, print_time)
@@ -138,6 +154,15 @@ class SERVO42C:
     def set_torque(self, torque, print_time):
         val = round(torque)
         self.mcu_s42c.set_register("SET_TORQ", val, print_time)
+
+    def set_kp(self, kp, print_time):
+        self.mcu_s42c.set_register("SET_KP", kp, print_time)
+
+    def set_ki(self, ki, print_time):
+        self.mcu_s42c.set_register("SET_KI", ki, print_time)
+
+    def set_kd(self, kd, print_time):
+        self.mcu_s42c.set_register("SET_KD", kd, print_time)
 
     cmd_SET_SERVO_CURRENT_help = "Set the current of a Servo42C driver"
 
